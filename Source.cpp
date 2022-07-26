29be205846cbd422e8b9b565a460b7be731f16fe
SeriousHyena <emackiii@gmail.com>
2022-07-26 10:26:25 -0400

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,26 @@
+/*Created 7 / 26 / 2022
+ Ernest Mack
+A simple program to calculate the mean height above high tide.
+User inputs unlimited grade heights.
+The program then calculates the mean height based on those inputs.
+Outputs Mean High Tide Elevation.
+*/
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main() {
+
+	double elev = 0;
+	str
+
+	cout<<  "Please enter a grade height: ";
+
+
+	cout<< ("Mean Grade above High Tide is: ") << elev << " Feet M.S.L.";
+	return 0;
+}
+
+
